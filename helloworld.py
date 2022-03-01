68a138bbe082b9f91e48c52a0146279bd99802b5
vinmahesh7477 <100726079+vinmahesh7477@users.noreply.github.com>
2022-03-01 15:26:35 -0500

    Hello world file commit

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+if 5 > 2:
+ print("five is greater than two")
+ print("checking indentation block error")
+ 
+
+x= 10
+y= "sample string"
+
+#printing the values of variables
+print(x)
+print(y) 
+
+"""
+checking for multi line quotes
+to see if the compiler will ignore items
+"""
+
+print("hello after multi line comment")
